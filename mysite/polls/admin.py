e51e9f98a77c6548a1d128f1c73dc53f925a9fb4
unknown <eunice_galvez_10@hotmail.com>
2020-08-22 18:11:00 -0500

    models functions added and admin view implemented

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Question, Choice
+# tell the admin Question table has an admin interface
+admin.site.register(Question)
+admin.site.register(Choice)
